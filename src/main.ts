c6caaee53a48b2a45a1a1c9aefc93e7628772116
André Schlichting <andresch.dev@gmail.com>
2025-09-16 11:11:08 -0300

    Remove old comment at main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,4 @@
 import { startGame } from "./game";
 import { assetsQuery } from "./preload";
 
-assetsQuery.then(() => startGame());
-
-/*
-todo: insert player in the grid
-renderer should track grid updates
-
-*/
+assetsQuery.then(startGame);
