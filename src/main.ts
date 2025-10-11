f00aeb91bd286b3588f91b2d329ca7b734dc10e9
André Schlichting <andresch.dev@gmail.com>
2025-10-11 19:51:57 -0300

    Fix white rook lazy initialization

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { startGame } from "./game";
 import { assetsQuery } from "./preload";
+import { css } from "./styles";
 
-assetsQuery.then(startGame);
+assetsQuery.then((assets) => {
+  promotionLazyInitialization(assets);
+  startGame();
+});
+
+function promotionLazyInitialization(assets: HTMLImageElement[] = []): void {
+  const whiteRookImage = assets.find(image => image.src.includes("white-rook"))!;
+  css.insertRule(`.player.promoted::after { --white-rook-url: url("${whiteRookImage.src}"); }`);
+}
